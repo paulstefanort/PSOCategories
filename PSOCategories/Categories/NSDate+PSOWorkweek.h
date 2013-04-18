0d4fca1915cc3033f60544c185b15d913579cfc6
Paul Stefan Ort <mail@paulstefanort.com>
2013-04-17 21:41:56 -0500

    Add stub for NSDate+PSOWorkweek.

---
+++ PSOCategories/Categories/NSDate+PSOWorkweek.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+PSOWorkweek.h
+//  PSOCategories
+//
+//  Created by Paul Stefan Ort on 4/17/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (PSOWorkweek)
+
+@end
---
+++ PSOCategories/Categories/NSDate+PSOWorkweek.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+PSOWorkweek.m
+//  PSOCategories
+//
+//  Created by Paul Stefan Ort on 4/17/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import "NSDate+PSOWorkweek.h"
+
+@implementation NSDate (PSOWorkweek)
+
+@end
