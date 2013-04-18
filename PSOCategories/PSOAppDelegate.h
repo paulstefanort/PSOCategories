b38736bce675fc02ebb72286d5af1283011235fb
Paul Stefan Ort <mail@paulstefanort.com>
2013-04-17 21:38:00 -0500

    Add iOS project.

---
+++ PSOCategories/PSOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSOAppDelegate.h
+//  PSOCategories
+//
+//  Created by Paul Stefan Ort on 4/17/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PSOCategories/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PSOCategories
+//
+//  Created by Paul Stefan Ort on 4/17/13.
+//  Copyright (c) 2013 Paul Stefan Ort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSOAppDelegate class]));
+    }
+}
