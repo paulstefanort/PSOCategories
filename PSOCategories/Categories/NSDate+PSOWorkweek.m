41c18af925655b07ee299f4a088bd4425819d49f
Paul Stefan Ort <mail@paulstefanort.com>
2013-04-19 23:22:02 -0500

    Use the current calendar instead of new (Gregorian) calendar.

--- PSOCategories/Categories/NSDate+PSOWorkweek.m
+++ PSOCategories/Categories/NSDate+PSOWorkweek.m
@@ -14,8 +14,7 @@
 @implementation NSDate (PSOWorkweek)
 
 - (BOOL)isWeekday {
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-    NSDateComponents *dateComponents = [calendar components:(NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:self];
+    NSDateComponents *dateComponents = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:self];
     NSInteger weekDay = [dateComponents weekday];
     
     if (weekDay == SATURDAY || weekDay == SUNDAY) {
@@ -25,49 +24,45 @@
 }
 
 - (NSDate *)nextWeekday {
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-    
     NSDateComponents *oneDayComponents = [NSDateComponents new];
     oneDayComponents.day = 1;
     NSDateComponents *twoDayComponents = [NSDateComponents new];
     twoDayComponents.day = 2;
     
     // begin by adding one day
-    NSDate *date = [calendar dateByAddingComponents:oneDayComponents toDate:self options:0];
+    NSDate *date = [[NSCalendar currentCalendar] dateByAddingComponents:oneDayComponents toDate:self options:0];
     
-    NSDateComponents *dateComponents = [calendar components:(NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:date];
+    NSDateComponents *dateComponents = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:date];
     NSInteger weekDay = [dateComponents weekday];
     
     if (weekDay == SATURDAY) {
         // add two days
-        date = [calendar dateByAddingComponents:twoDayComponents toDate:date options:0];
+        date = [[NSCalendar currentCalendar] dateByAddingComponents:twoDayComponents toDate:date options:0];
     } else if (weekDay == SUNDAY) {
         // add one day
-        date = [calendar dateByAddingComponents:oneDayComponents toDate:date options:0];
+        date = [[NSCalendar currentCalendar] dateByAddingComponents:oneDayComponents toDate:date options:0];
     }
     return date;
 }
 
 - (NSDate *)previousWeekday {
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-    
     NSDateComponents *oneDayComponents = [NSDateComponents new];
     oneDayComponents.day = -1;
     NSDateComponents *twoDayComponents = [NSDateComponents new];
     twoDayComponents.day = -2;
     
     // begin by subtracting one day
-    NSDate *date = [calendar dateByAddingComponents:oneDayComponents toDate:self options:0];
+    NSDate *date = [[NSCalendar currentCalendar] dateByAddingComponents:oneDayComponents toDate:self options:0];
     
-    NSDateComponents *dateComponents = [calendar components:(NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:date];
+    NSDateComponents *dateComponents = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit) fromDate:date];
     NSInteger weekDay = [dateComponents weekday];
     
     if (weekDay == SATURDAY) {
         // subtract one day
-        date = [calendar dateByAddingComponents:oneDayComponents toDate:date options:0];
+        date = [[NSCalendar currentCalendar] dateByAddingComponents:oneDayComponents toDate:date options:0];
     } else if (weekDay == SUNDAY) {
         // subtract two days
-        date = [calendar dateByAddingComponents:twoDayComponents toDate:date options:0];
+        date = [[NSCalendar currentCalendar] dateByAddingComponents:twoDayComponents toDate:date options:0];
     }
     return date;
 }
